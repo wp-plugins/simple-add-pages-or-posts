84347db413640be6c4b305bd47523747d2b32f28
ramon fincken <ramon fincken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-11 20:46:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-add-pages-or-posts/trunk@204336 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Admin renamer extended ===
+Contributors: Simon Dirlik, Ramon Fincken
+Donate link: http://donate.ramonfincken.com
+Tags: admin,admins,add,mass,simple,easy,post,posts,page,pages
+Requires at least: 2.0.2
+Tested up to: 2.9.1
+Stable tag: 1.0
+
+Plugin to simply add posts or pages by entering their titles. Handy tool if you have a new blog and want to add pages like Home, About, Contact ...
+
+== Description ==
+
+Plugin to simply add posts or pages by entering their titles. Handy tool if you have a new blog and want to add pages like Home, About, Contact ...
+
+== Installation ==
+
+1. Upload directory `simple-add-pages-or-posts` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Visit Plugins menu to add your post(s) or page(s).
+
+== Frequently Asked Questions ==
+
+None available
+
+
+== Changelog ==
+
+= 1.0 =
+First release
+
+
+== Screenshots ==
+
+1. Enter title
+2. After submit
---
+++ sapp.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Simple add pages or posts.
+ *
+ * @category      Wordpress Plugins
+ * @package       Plugins
+ * @author        Simon Dirlik, Ramon Fincken
+ * @copyright     Yes, Open source
+ * @version       v 1.0
+*/
+if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
+
+/**
+ * Displays the admin page
+ */
+function plugin_sapp_initpage() {
+   if (isset ($_GET['page']) && $_GET['page'] == 'simple-add-pages-or-posts/sapp.php') {
+      require_once PLUGIN_SAPP_DIR . '/form.php';
+   }
+}
+
+/**
+ * Additional links on the plugin page
+ */
+function plugin_sapp_RegisterPluginLinks($links, $file) {
+   if ($file == 'simple-add-pages-or-posts/simple_add_pages_or_posts.php') {
+      $links[] = '<a href="plugins.php?page=simple-add-pages-or-posts/sapp.php">' . __('Settings') . '</a>';
+      $links[] = '<a href="http://donate.ramonfincken.com">' . __('Donate') . '</a>';
+      $links[] = '<a href="http://www.creativepulses.nl">' . __('Custom WordPress coding nodig?') . '</a>';      
+   }
+   return $links;
+}
+
+add_filter('plugin_row_meta','plugin_sapp_RegisterPluginLinks', 10, 2);
+
+/**
+ * Left menu display in Plugin menu
+ */
+function plugin_sapp_addMenu() {
+   add_submenu_page("plugins.php", "Simple add pages or posts", "Simple add pages and posts", 10, __FILE__, 'plugin_sapp_initpage');
+}
+
+add_action('admin_menu', 'plugin_sapp_addMenu');
+
+/**
+ * Loading the CSS file
+ *//*
+function plugin_find_replace_css() {
+   $admin_stylesheet_url = plugin_find_replace_plugin_url('styles/style.css');
+   echo '<link rel="stylesheet" href="' . $admin_stylesheet_url . '" type="text/css" />';
+}
+add_action('admin_head', 'plugin_find_replace_css');
+*/
+/**
+ * Generating the url for current Plugin
+ *
+ * @param String $path
+ * @return String
+ */
+function plugin_sapp_plugin_url($path = '') {
+   global $wp_version;
+
+   if (version_compare($wp_version, '2.8', '<')) { // Using WordPress 2.7
+      $folder = dirname(plugin_basename(__FILE__));
+      if ('.' != $folder)
+         $path = path_join(ltrim($folder, '/'), $path);
+      return plugins_url($path);
+   }
+   return plugins_url($path, __FILE__);
+}
+?>
